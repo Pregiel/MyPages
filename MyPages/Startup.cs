9d1a097ac7205d541d0bd7f7935d7c93e3f6991d
Pregiel <pregiel13@gmail.com>
2019-07-25 17:57:06 +0200

    Added README Prepared to deploy to Docker and Heroku. Removed Ordinal Number from view.

--- MyPages/Startup.cs
+++ MyPages/Startup.cs
@@ -75,12 +75,12 @@ namespace MyPages
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+            //else
+            //{
+            //    app.UseExceptionHandler("/Error");
+            //    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            //    app.UseHsts();
+            //}
 
             app.UseAuthentication();
             app.UseHttpsRedirection();
---
+++ README.md
@@ -0,0 +1,19 @@
+# MyPages (in development)
+
+Web service which allow user to create notes. Each note can contain other notes. 
+Informations are saved in [Markdown](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet).
+
+## Demo
+
+Check a live Demo here https://my-pages-web.herokuapp.com/
+
+Create new account or use this:
+* **Username**: test
+* **Password**: password
+
+## Features
+- Add/remove/edit page
+- Change order
+- Live preview during creating/editing page
+
+*More features coming soon.*
