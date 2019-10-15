7b8120f39316f977dc4a522351e398fbc314eaca
Pregiel <pregiel13@gmail.com>
2019-10-15 19:34:38 +0200

    Added colors to breadcrumb dropdown menus

---
+++ MyPages/Helpers/GlobalVariables.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyPages.Helpers
+{
+    public static class GlobalVariables
+    {
+        public static readonly string StyleVersion = "v=1.5";
+    }
+}
